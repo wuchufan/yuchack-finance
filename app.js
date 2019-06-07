b1727e1241615996ca8425ceb42268fdb62bee61
TIEC <TIEC@infos-iMac.local>
2019-06-07 16:28:17 -0400

    set up ejs

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
 
+const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
-  res.sendFile(__dirname + "/index.html")
+  res.render("main");
+
+  // res.sendFile(__dirname + "/index.html")
 });
+
+
 app.use(express.static("public"))
 app.listen(3000,function(){
   console.log("server is up at 3000");
