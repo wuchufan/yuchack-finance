f7eb099fc96fc5a75af2195ef2ae06e573b3b248
TIEC <TIEC@infos-iMac.local>
2019-06-24 16:23:23 -0400

    finished basic layout of website, working on posting blogs on news page

--- app.js
+++ app.js
@@ -1,27 +1,79 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
-app.get("/",function(req,res){
-  res.render("main");
 
-  // res.sendFile(__dirname + "/index.html")
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true
 });
 
-app.get("/startup",function(req,res){
+//mongoose schema
+const newsSchema = {
+  newsTitle: String,
+  newsDate: String,
+  newsBody: String,
+  newsLBBody: Array
+};
+
+//mongoose model
+const newsModel = mongoose.model("news", newsSchema);
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"))
+
+//home route
+app.get("/", function(req, res) {
+  res.render("main");
+});
+
+//startup route
+app.get("/startup", function(req, res) {
   res.render("startup");
 });
 
-app.get("/estate",function(req,res){
+//estate route
+app.get("/estate", function(req, res) {
   res.render("estate");
 });
-app.get("/news",function(req,res){
+
+//news route
+app.get("/news", function(req, res) {
   res.render("news");
 });
 
-app.use(express.static("public"))
-app.listen(3000,function(){
+app.get("/news/compose", function(req, res) {
+  const newsTitle = req.body.newsTitle; //title
+  const newsBody = req.body.newsBody; //article paragraphs
+  const newsLBBody = req.body.newsBody.split("\r\n"); // article paragraphs broke in seperated paragraphs
+
+  //getting today's date
+  var today = new Date();
+  var options = {
+    year:"numeric",
+    month:"long",
+    day:"numeric"
+  };
+  var day = today.toLocaleDateString("en-US",options);
+
+  const newNews = new newsModel ({
+    newsTitle: newsTitle,
+    newsDate: day,
+    newsBody: newsBody,
+    newsLBBody: newsLBBody
+  });
+  res.render("compose");
+})
+
+app.get("/edit", function(req, res) {
+  res.render("edit")
+})
+
+
+app.listen(3000, function() {
   console.log("server is up at 3000");
 });
