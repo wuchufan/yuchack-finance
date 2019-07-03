279beef6dd33867b1eff559488eaf10b3c5b7261
Jeffrey Wu <wuchufan@Jeffreys-MacBook-Pro.local>
2019-07-02 22:57:48 -0400

    finished adding articles, including function of deleting news article, working on editing news article

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 
 //gridfs
 const mongoURI = "mongodb://localhost:27017/newsImageDB";
-const conn = mongoose.createConnection(mongoURI,{
+const conn = mongoose.createConnection(mongoURI, {
   useNewUrlParser: true
 });
 mongoose.connect("mongodb://localhost:27017/newsDB", {
@@ -32,7 +32,7 @@ mongoose.connect("mongodb://localhost:27017/newsDB", {
 
 //connect GridFS and Mongo
 let gfs;
-conn.once('open', function(){
+conn.once('open', function() {
   // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploads');
@@ -50,7 +50,7 @@ const newsSchema = {
 //create storage engine
 const storage = new GridFsStorage({
   url: mongoURI,
-  options:{
+  options: {
     useNewUrlParser: true
   },
   file: (req, file) => {
@@ -70,7 +70,9 @@ const storage = new GridFsStorage({
   }
 });
 
-const upload = multer({storage});
+const upload = multer({
+  storage
+});
 
 //mongoose model
 const newsModel = mongoose.model("news", newsSchema);
@@ -96,13 +98,65 @@ app.get("/estate", function(req, res) {
 //news route
 app.get("/news", function(req, res) {
 
-  newsModel.find({},function(err,foundNews){
-    if (!err){
+  newsModel.find({}, function(err, foundNews) {
+    if (!err) {
       console.log(foundNews);
-      res.render("news",{
-        foundNews:foundNews //a list, elements are objects
+      res.render("news", {
+        foundNews: foundNews //a list, elements are objects
       });
     }
+  });
+});
+//route: GET /news/single/:post/delete
+//desc: going to individual news post
+
+app.get("/news/single/:post/delete", function(req, res) {
+  //getting image filename
+  newsModel.findById({
+    _id: req.params.post
+  }, function(err, foundNews) {
+    //check weather image is uploaded
+    if (foundNews.newsImage !== 'No Image Uploaded'){
+      //removing image from Gridfs
+      gfs.remove({
+        filename: foundNews.newsImage,
+        root: 'uploads'
+      }, (err, gridStore) => {
+        if (err) {
+          return res.status(404).json({
+            err: err
+          });
+        }
+      });
+    }
+
+  });
+
+  //removing article from MongoDb
+  newsModel.findOneAndDelete({
+    _id: req.params.post
+  }, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.redirect("/news");
+});
+
+//route: GET /news/single/:post
+//desc: going to individual news post
+app.get("/news/single/:post", function(req, res) {
+  newsModel.findById({
+    _id: req.params.post
+  }, function(err, foundNews) {
+    // console.log(foundCompose);
+
+    res.render("news-post", {
+      newsTitle: foundNews.newsTitle,
+      newsDate: foundNews.newsDate,
+      newsLBBody: foundNews.newsLBBody,
+      newsImage: foundNews.newsImage
+    });
   });
 });
 
@@ -110,15 +164,27 @@ app.get("/news/compose", function(req, res) {
   res.render("compose");
 });
 
-app.post("/news/compose",upload.single('file1'),function(req,res){
+app.post("/news/compose", upload.single('file1'), function(req, res) {
 
-  const newsTitle = req.body.newsTitle; //title
-  const newsBody = req.body.newsBody; //article paragraphs
-  const newsLBBody = req.body.newsBody.split("\r\n"); // article paragraphs broke in seperated paragraphs
-  const newsImage = req.file.filename; //give name of uploaded file/image
+  //title
+  const newsTitle = req.body.newsTitle;
+  //article paragraphs
+  const newsBody = req.body.newsBody;
+  // article paragraphs broke in seperated paragraphs
+  const newsLBBody = req.body.newsBody.split("\r\n");
+  var newsImage;
+  // check if there's input
+  if (typeof(req.file) !== 'undefined') {
+    //give name of uploaded file/image
+    newsImage = req.file.filename;
+  } else {
+    newsImage = 'No Image Uploaded';
+  }
+
+
   var day = getTodayDate();
 
-  const newNews = new newsModel ({
+  const newNews = new newsModel({
     newsTitle: newsTitle,
     newsDate: day,
     newsBody: newsBody,
@@ -126,8 +192,8 @@ app.post("/news/compose",upload.single('file1'),function(req,res){
     newsImage: newsImage
   });
 
-  newNews.save(function(err,doc){
-    if (!err){
+  newNews.save(function(err, doc) {
+    if (!err) {
       console.log("New news saved succeessful");
     } else {
       console.log(err);
@@ -138,13 +204,13 @@ app.post("/news/compose",upload.single('file1'),function(req,res){
 
 //@route GET /files
 //@desc Display all files in JSON
-app.get("/files",function(req,res) {
+app.get("/files", function(req, res) {
 
-  gfs.files.find().toArray(function(err,files) {
+  gfs.files.find().toArray(function(err, files) {
     //check if files
-    if(!files || files.length===0){
+    if (!files || files.length === 0) {
       res.status(404).json({
-        err:"no files"
+        err: "no files"
       });
     } else {
       //file exists
@@ -158,17 +224,19 @@ app.get("/files",function(req,res) {
 //@desc display single file object
 
 
-app.get("/news/image/:filename",function(req,res) {
+app.get("/news/image/:filename", function(req, res) {
 
-  gfs.files.findOne({filename:req.params.filename},function(err,file){
+  gfs.files.findOne({
+    filename: req.params.filename
+  }, function(err, file) {
     //check if file
-    if (!file || file.length ===0){
+    if (!file || file.length === 0) {
       return res.status(404).json({
-        err:'no file exists'
+        err: 'no file exists'
       });
     }
     // check if image
-    if (file.contentType==='image/jpeg' || file.contentType === 'image/png'){
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
       //Read output to browser
       const readstream = gfs.createReadStream(file.filename);
       readstream.pipe(res);
@@ -180,12 +248,9 @@ app.get("/news/image/:filename",function(req,res) {
   });
 });
 
-app.get("/news/post",function(req,res){
-  res.render("news-post");
-});
 
 
-app.get("/edit", function(req, res) {
+app.get("/news/single/:post/edit", function(req, res) {
   res.render("edit");
 });
 
@@ -200,14 +265,14 @@ app.listen(3000, function() {
 
 
 //getting today's date
-function getTodayDate(){
+function getTodayDate() {
 
   var today = new Date();
   var options = {
-    year:"numeric",
-    month:"long",
-    day:"numeric"
+    year: "numeric",
+    month: "long",
+    day: "numeric"
   };
 
-  return today.toLocaleDateString("en-US",options);
+  return today.toLocaleDateString("en-US", options);
 }
